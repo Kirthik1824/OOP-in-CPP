f0cab4ec6c158b4040fc75a9b96382a0f7b71c1b
Kirthik <kirthik.nitt@gmail.com>
2025-07-21 21:29:14 +0530

    abstraction-abstract-class

--- Basics.cpp
+++ Basics.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-using std :: string;
+using namespace std;
 
+class abstractEmployee {    
+public:
+    virtual void askForPromotion() = 0; // pure virtual function
+};
 class Employee {
-    string name;
-    string company;
-    
+public:
+    string Name;
+    string Company;
+    int Age;
+    Employee(string name, string company, int age) {
+        Name=name;
+        Company=company;
+        Age=age;
+    }
+    void askForPromotion() {
+        if (Age > 30) {
+            cout << Name << " got promoted!" << endl;
+        } else {
+            cout << Name << ", sorry no promotion for you!" << endl;
+        }
+    }
 };
 
 int main () {
-    
+    Employee employee1=Employee("John", "Google", 35);
+    employee1.askForPromotion();
+    cout << "Employee Name: " << employee1.Name << endl;
 }
